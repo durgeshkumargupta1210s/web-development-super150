adae26f35a3c6deff2ca0e25af8ab3e8ac854c8d
Durgesh Kumar Gupta <durgeshkumargupta1210s@gmail.com>
2025-06-11 23:01:35 +0530

    lecture 44

--- Node js/Session 44/expressBasic/index.js
+++ Node js/Session 44/expressBasic/index.js
@@ -3,5 +3,11 @@
 
 const express = require('express') //  retuns a function
 const app = express();// returns an entire new object
-console.log(app);
+// console.log(app);
 
+//ask server to listen to your requests
+app.listen(8080,()=>{
+    console.log("server connected at port 8080");
+});
+
+// app.listen(8080);
