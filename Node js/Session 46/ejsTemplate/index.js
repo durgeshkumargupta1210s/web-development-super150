9d5b39d83db40beca17f9b1f61d99c06993a9ece
Durgesh Kumar Gupta <durgeshkumargupta1210s@gmail.com>
2025-06-20 08:46:23 +0530

    ejsTemplate and QueryString

--- Node js/Session 46/ejsTemplate/index.js
+++ Node js/Session 46/ejsTemplate/index.js
@@ -1,17 +1,33 @@
+const express = require('express');
+const app = express();
+const path = require('path');
 
-const express=require('express')
-const app=express();
+// Setting EJS as the view engine
+app.set('view engine', 'ejs');
+// Setting the directory where your EJS templates are located
+app.set('views', path.join(__dirname, 'views')); // ✅ fixed 'views'
 
-app.set('view engine','ejs')// view engine ko set kar diya hai taki template dekh pawoo
+let todos=['go to gym', 'go to club', 'drink water']
 
+// Root route
+app.get('/', (req, res) => {
+    res.render('index'); // renders views/index.ejs
+});
 
-app.get('/',(req,res)=>{
-    // res.send("hii")
-    res.render('index');
+// /random route
+app.get('/random', (req, res) => {
+    let number=Math.ceil(Math.random()*100)
+
+    res.render('random', {number}); // renders views/random.ejs
+});
+
+// show your tast
+
+app.get('/todo',(req,res)=>{
+       res.render("todos",{todos})
 })
 
-
-
-app.listen(8080,()=>{
-    console.log("server connected to 8080 port")
-})
+// Start the server
+app.listen(8080, () => {
+    console.log("Server connected to port 8080");
+});
