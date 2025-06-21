b2b96aa6fa069f6e08edd27db7bcd7cbb21cd8fa
Durgesh Kumar Gupta <durgeshkumargupta1210s@gmail.com>
2025-06-21 07:33:43 +0530

    getVsPost StaticFiles partialFiles

--- Node js/Session 47/getVsPost/index.js
+++ Node js/Session 47/getVsPost/index.js
@@ -8,9 +8,27 @@ app.set('view engine','ejs')
 app.set('views',path.join(__dirname,'views'))// templating file
 app.use(express.static(path.join(__dirname,'public')))//  static files
 
+app.use(express.urlencoded({ extended: true })); // for form data
+app.use(express.json()); // for JSON data
+
 app.get('/',(request,response)=>{
     response.render('index')
 })
+
+app.get('/user',(request,response)=>{
+    console.log(request.query)
+    let {username,age}=request.query
+    response.send(`Hi !! i my name is ${username} and my gae is ${age}`)
+    // response.send('<h3>From get method</h3>')
+})
+
+app.post('/user',(request,response)=>{
+    console.log(request.body)
+    let {username,age}=request.body
+    response.send(`Hi !! i my name is ${username} and my gae is ${age}`)
+    // response.send('<h3>From post method</h3>')
+})
+
 
 
 
