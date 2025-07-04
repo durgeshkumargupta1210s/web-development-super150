ab725e071aa3990183995e862ab3956e5bd88367
Durgesh Kumar Gupta <durgeshkumargupta1210s@gmail.com>
2025-07-04 08:08:53 +0530

    Day-3 Product-review-functionality and middleware-of-mongoose

---
+++ Node js/Session 56/routes/review.js
@@ -0,0 +1,24 @@
+const express=require('express')
+
+const router=express.Router()// mini instance
+
+const Review=require('../Models/Review')
+const Product=require('../Models/Product')
+
+
+router.post('/products/:id/review',async (req,res)=>{
+    console.log(req.body);
+    console.log(req.params);
+    let {id}=req.params;
+    let{rating, comment}=req.body;
+    const product= await Product.findById(id);
+    // creating entry in the review database by using js instead of mongodb
+    const review= new Review({rating, comment});
+    // adding review into product database
+    product.reviews.push(review);
+   await  review.save();// save data into database
+   await product.save();
+   res.redirect(`/products/${id}`)
+})
+
+module.exports=router;
