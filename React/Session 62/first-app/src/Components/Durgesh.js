992e51179e5f309f1186d93549fd9e6491e75a1b
Durgesh Kumar Gupta <durgeshkumargupta1210s@gmail.com>
2025-08-29 00:19:33 +0530

    React-basic , props, webpack

--- React/Session 62/first-app/src/Components/Durgesh.js
+++ React/Session 62/first-app/src/Components/Durgesh.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 let  arr2=["😍","❤️","🤣" ,"💕", "👍","🙌","😒","😘","👌"];
 let emoji=Math.floor(Math.random()*9);
-let todos=["day1", "day2", "day3","day4", "day5", "day6","day7","day8", "day9"]
+let todos=["day1", "day2", "day3","day4", "day5", "day6","day7","day8"]
 const Durgesh = () => {
   return (
     <div>
